819b5853922f505f24f7486d68471af1d5e78f1b
Utkarsh2902 <69791796+Utkarsh2902@users.noreply.github.com>
2023-06-17 14:29:57 +0000

    callback function

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-//function declaration
-function harry() {
-  console.log(`hey yo`);
+//callback functions
+function money(id){
+console.log(`my name is utkarsh ${id}` )
+} 
+
+function reg(num1,num2,money){
+  let sum=num1+num2;
+  money(sum);
+  console.log(`need your id ${sum}`)
 }
-harry();
-
-// function expression
-const harry1=function(){
-  console.log(`hey duplicate`)
-}
-harry1();
-
-//arrow function better version of extression
-const harry2=(a,b)=>a+b;
-console.log(harry2(1,1))
-
-
-
+reg(6,7,money);
