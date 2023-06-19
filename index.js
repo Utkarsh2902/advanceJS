159a0c6d658014ba9006bcf8d1c43038a027f265
Utkarsh2902 <69791796+Utkarsh2902@users.noreply.github.com>
2023-06-19 01:29:00 +0000

    call backs and setTimeout example

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
-let str ="frontenddeveloper"
-//index('substring',position fron beg)
 
-//alert(str.indexOf('en',12));
-//index('substring',position fron end)
+//call back function with setTimeout and which leads to callback hell 
+// better use promises
+let stocks = {
+  Fruits: ["strawberry", "grapes", "banana", "apple"],
+  liquid: ["water", "ice"],
+  holder: ["cone", "cup", "stick"],
+  toppings: ["chocolate", "peanuts"],
+};
 
-//alert(str.lastIndexOf('r',28))
- 
-//includes
-//alert("hi".includes('h'))
-//startsWith
-//alert("frontend".startsWith('s'))
-//endsWith
-//alert("developer".endsWith('r'))
-//slice
-//alert(str.slice(1,5))
-//substring
-//alert(str.substring(4,2) )
-//substr
-//alert(str.substr(-5,3))
+let order = (fruit_name, call_production) => {
+  setTimeout(() => {
+    console.log(`your order ${stocks.Fruits[fruit_name]}`)
+    setTimeout(() => {
+      console.log("cut the fruit")
+      setTimeout(() => {
+        console.log(`add ${stocks.liquid[0]} and ${stocks.liquid[1]}`)
+        setTimeout(()=>{
+          console.log("start the machine")
+          setTimeout(()=>{
+           console.log(`select container ${stocks.holder[0]}`) 
+            setTimeout(()=>{
+              console.log(`select toppings ${stocks.toppings[0]} `)
+              setTimeout(()=>{
+                console.log("ice cream is ready")
+              },2000)
+            },3000)
+          },2000)
+        },1000)
+      }, 1000)
+    }, 2000)
+    call_production();
+  }, 2000)
+
+}
+let production = () => {
+  console.log("go for production")
+}
+order("0", production)
+
+
+// setTimeout(()=>{
+//   console.log("hey after 2 sec")
+// },2000)
+// setTimeout(()=>{
+//   console.log("hey after 2 sec")
+// },2000)
+//   setTimeout(()=>{
+//   console.log("hey after 2 sec")
+// },0000)
