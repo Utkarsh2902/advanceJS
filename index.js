f1d213927dc535db1cbd3af723cd850cb876c284
Utkarsh2902 <69791796+Utkarsh2902@users.noreply.github.com>
2023-07-26 15:31:01 +0000

    top coding output JS questions

--- index.js
+++ index.js
@@ -1,28 +1,67 @@
-//api example
+// Top coding output based interview questions
 
-// var URL="https://api.funtranslations.com/translate/yoda.json?text=I am Utkarsh"
-// fetch(URL)
-// .then(response=>response.json())
-// .then(json=>console.log(json))
-
-
-//class and objects
-
-// class car{
-
-//   constructor(name,no)
-//   {
-//     this.name=name;
-//     this.no=no;
-//   }
-//  area()
-//   {
-//      console.log(this.name+this.no)
-//   }  
+//hoistring works with var not with let and const
+// function name(){
+//   console.log(old);
+//   console.log(young);
+//  console.log(mid);
+//   var old=50;   //undefined
+// let young=5;    //err 
+//   const mid=25;  //err
 // }
-// const label=new car("utkarsh",10)
-// const label1=new car("priyanshi",10)
+// name()
 
 
-// label.area()
-// label1.area()
+
+
+//nature of var is global
+// for(var i=0;i<3;i++)
+//   {
+//     setTimeout(()=>console.log(i),1)
+//   }
+
+
+//nature of let is blocked
+// for(let i=0;i<3;i++)
+//   {
+//     setTimeout(()=>console.log(i),1)
+//   }
+
+//console questions
+
+// console.log(+false)
+// console.log(+true)
+// console.log(typeof +false)
+// console.log(typeof +true)
+
+//console.log(+"true")
+// console.log(! true)
+// console.log(typeof !true)
+//console.log(typeof "!true")
+
+//same and different identifers
+// var x=10;
+// var x=11;
+// // let x=50
+// console.log(x)
+
+
+//act as string
+// function hero(a,b){
+//   return a+b;
+// }
+// console.log(hero(1,"5"))
+
+//increment
+
+// let number=0;
+// let number1=0;
+// console.log(number++);
+// console.log(++number);
+// console.log(number);
+// console.log(--number1);
+// console.log(number1--);
+// console.log(number1);
+
+
+//**The type of array is object
